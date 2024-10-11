a227137f4076eadbf4f45812732e139d3389a9bb
姜鸣鹤 <jiang.minghe@byd.com>
2024-10-11 09:00:05 +0800

    refactor: 添加pkg包说明

--- internal/pkg/doc.go
+++ internal/pkg/doc.go
@@ -1,1 +1,14 @@
+/*
+Package pkg 包含了项目的公共类部分。具体地：
+
+config.go -- 统一定义了所有配置的加载项，便于使用
+
+logger.go -- 配置logger项
+
+以下两项因为在多个模块共用，故放置在此包中
+
+point.go -- 数据点模型定义
+
+snapshot.go -- 设备快照模型定义
+*/
 package pkg
