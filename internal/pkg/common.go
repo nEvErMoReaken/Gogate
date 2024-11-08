28e7a3ff14c6f8d793723635919653a5dfd786d9
姜鸣鹤 <jiang.minghe@byd.com>
2024-11-08 14:28:07 +0800

    feature/使用io.pipe改写tcp流式处理逻辑: 定义离散数据源和流式数据源，用于统一数据源接口

--- internal/pkg/common.go
+++ internal/pkg/common.go
@@ -1,6 +1,7 @@
 package pkg
 
 import (
+	"io"
 	"time"
 )
 
@@ -13,7 +14,62 @@ type Point struct {
 }
 
 // DataSource 是Connector和Parser之间传递的数据结构
-type DataSource struct {
-	Source   interface{}
-	MetaData map[string]interface{}
+type DataSource interface {
+	Type() string // 用于标识数据源类型
+}
+
+// StreamDataSource 实现了 StreamSource 接口
+type StreamDataSource struct {
+	reader *io.Reader
+	writer *io.Writer
+}
+
+// NewStreamDataSource 使用指定的 reader 和 writer 创建 StreamDataSource 实例
+func NewStreamDataSource(reader *io.Reader, writer *io.Writer) *StreamDataSource {
+	return &StreamDataSource{
+		reader: reader,
+		writer: writer,
+	}
+}
+
+func (s *StreamDataSource) Type() string {
+	return "stream"
+}
+
+// ReadFully 阻塞读取直到缓冲区填满
+func (s *StreamDataSource) ReadFully(p []byte) (int, error) {
+	return io.ReadFull(*s.reader, p)
+}
+
+// WriteASAP 立即写入数据
+func (s *StreamDataSource) WriteASAP(data []byte) (int, error) {
+	return (*s.writer).Write(data)
+}
+
+// MessageDataSource 实现了 MessageSource 接口
+type MessageDataSource struct {
+	dataChan chan []byte
+}
+
+func (m *MessageDataSource) Type() string {
+	return "message"
+}
+
+// ReadOne 从通道中读取一个完整的数据包
+func (m *MessageDataSource) ReadOne() ([]byte, error) {
+	data, ok := <-m.dataChan
+	if !ok {
+		return nil, io.EOF
+	}
+	return data, nil
+}
+
+// WriteOne 从通道中读取一个完整的数据包
+func (m *MessageDataSource) WriteOne(data []byte) error {
+	// 如果通道已关闭，返回 EOF
+	if m.dataChan == nil {
+		return io.EOF
+	}
+	m.dataChan <- data
+	return nil
 }
